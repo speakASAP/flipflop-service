c34ca10f0daaf185e20b782a1d61a306f72107e7
Sergey SpeakASAP <stashok@speakasap.com>
2025-12-26 14:19:59 +0100

    Add ClientsModule to app.module.ts in cart, order, and warehouse services

--- services/warehouse-service/src/app.module.ts
+++ services/warehouse-service/src/app.module.ts
@@ -5,7 +5,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { WarehouseModule } from './warehouse/warehouse.module';
-import { HealthModule, PrismaModule, LoggerModule, AuthModule } from '@flipflop/shared';
+import { HealthModule, PrismaModule, LoggerModule, AuthModule, ClientsModule } from '@flipflop/shared';
 import { HealthController } from './health/health.controller';
 
 @Module({
@@ -17,6 +17,7 @@ import { HealthController } from './health/health.controller';
     PrismaModule,
     LoggerModule,
     AuthModule,
+    ClientsModule,
     HealthModule,
     WarehouseModule,
   ],
